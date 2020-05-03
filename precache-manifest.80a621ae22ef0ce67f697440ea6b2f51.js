60047c71ad8c17f95790178013159bae921de3fb
HatzoriHanzo <victorpierre155@gmail.com>
2020-05-02 23:51:05 -0300

    Updates

---
+++ precache-manifest.80a621ae22ef0ce67f697440ea6b2f51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45bf28c5f1fc86d85dd70bc2d92dae00",
+    "url": "/react-js-app-githubpage/index.html"
+  },
+  {
+    "revision": "f6d8378a1b906dcea857",
+    "url": "/react-js-app-githubpage/static/css/main.e6b8d45b.chunk.css"
+  },
+  {
+    "revision": "760659e0fcb98359db3d",
+    "url": "/react-js-app-githubpage/static/js/2.a442f27a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-js-app-githubpage/static/js/2.a442f27a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6d8378a1b906dcea857",
+    "url": "/react-js-app-githubpage/static/js/main.b275b6f8.chunk.js"
+  },
+  {
+    "revision": "c73e3ccece4ccfb2a526",
+    "url": "/react-js-app-githubpage/static/js/runtime-main.545d94f2.js"
+  },
+  {
+    "revision": "803d982e23078268a1606851ed66dfbe",
+    "url": "/react-js-app-githubpage/static/media/image.803d982e.png"
+  }
+]);
